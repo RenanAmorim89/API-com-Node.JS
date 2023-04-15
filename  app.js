a526c4f9df7666768fdad1be8cca219d41d2074e
Renan Amorim <renanamorim89@hotmail.com>
2023-04-15 17:50:49 -0300

    Passo 3 - Melhorando o Projeto e Tratando Erros

---  app.js
+++  app.js
@@ -1,10 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
-const rotaProdutos = require('./routes/produtos.js');
-const rotaPedidos = require('./routes/pedidos.js');
+const rotaProdutos = require("./routes/produtos.js");
+const rotaPedidos = require("./routes/pedidos.js");
 
-app.use('/produtos', rotaProdutos);
-app.use('/pedidos', rotaPedidos);
+app.use(morgan("deve"));
 
+app.use("/produtos", rotaProdutos);
+app.use("/pedidos", rotaPedidos);
+
+//Quando não encontra rota
+app.use((req, res, next) => {
+  const erro = new Error("Não econtrado");
+  erro.status = 404;
+  next(erro);
+});
+
+app.use((req, res, next) => {
+  res.status(erro.status || 500);
+  return res.send({
+    erro: {
+      mensagem: error.message,
+    },
+  });
+});
 module.exports = app;
